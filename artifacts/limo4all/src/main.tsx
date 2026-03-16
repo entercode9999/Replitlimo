60560faf815af2e365e9feeda21696636956c0fd
entercode9999 <habibiss@icloud.com>
2026-03-15 22:46:45 -0400

    Add SEO, schema markup, and prerender support

--- artifacts/limo4all/src/main.tsx
+++ artifacts/limo4all/src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HelmetProvider>
+    <Router base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <App />
+    </Router>
+  </HelmetProvider>,
+);
